8791f2663d97204430cd3e3a98c8804d1919232c
Moreno2825 <rodrigocaraveo14@gmail.com>
2024-06-20 17:16:27 -0600

    fix(); correccion

--- src/middleware/verifySignUp.js
+++ src/middleware/verifySignUp.js
@@ -3,6 +3,6 @@ import User from "../models/user.js";
 export const verifyDuplicateEmail = async (req, res) => {
   const user = User.findOne({ email: req.body.email });
 
-  if(user)
+  if(!user)
     return res.status(400).json({message:"The email is already use"})
 };
